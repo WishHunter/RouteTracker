7f4e8dbee0cd30343aa62a433494de28c827ca82
d.molkov <d.molkov@fbs.com>
2021-11-28 23:57:42 +0300

    HW-7: add Notification

--- RouteTracker/AppDelegate.swift
+++ RouteTracker/AppDelegate.swift
@@ -7,44 +7,62 @@
 
 import UIKit
 import GoogleMaps
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyA-Ph65XVA_gqT3JA0C4Tf9CwaY8GfDLXs")
-        
-        let controller: UIViewController
-        
-        if UserDefaults.standard.bool(forKey: "isLogin") {
-            controller = UIStoryboard(name: "Main", bundle: nil)
-                .instantiateViewController(MapViewController.self)
-        } else {
-            controller = UIStoryboard(name: "AuthStoryboard", bundle: nil)
-                .instantiateViewController(AuthViewController.self)
-        }
-        
-        window = UIWindow()
-        window?.rootViewController = UINavigationController(rootViewController: controller)
-        
-        window?.makeKeyAndVisible()
-        
-        return true
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyA-Ph65XVA_gqT3JA0C4Tf9CwaY8GfDLXs")
+
+    let controller: UIViewController
+
+    if UserDefaults.standard.bool(forKey: "isLogin") {
+      controller = UIStoryboard(name: "Main", bundle: nil)
+        .instantiateViewController(MapViewController.self)
+    } else {
+      controller = UIStoryboard(name: "AuthStoryboard", bundle: nil)
+        .instantiateViewController(AuthViewController.self)
     }
 
-    // MARK: UISceneSession Lifecycle
+    window = UIWindow()
+    window?.rootViewController = UINavigationController(rootViewController: controller)
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    window?.makeKeyAndVisible()
+
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .sound, .badge]) {
+      granted, error in
+
+      guard granted else {
+        print("Разрешение не получено")
+        return
+      }
+
+      let secondsToShow: Double = Double(30 * 60)
+
+      LocalNotification.sendNotificationRequest(
+        title: "You left me",
+        body: "Come back, I'm cold and lonely",
+        secondsToShow: secondsToShow)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+    return true
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
 }
